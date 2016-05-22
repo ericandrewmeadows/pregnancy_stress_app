bba5cd9218640759dee5408056ac7758d94e15b0
Eric Meadows <eric@practo.com>
2016-05-22 09:53:31 -0700

    Initial Commit

---
+++ Calmlee/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Calmlee
+//
+//  Created by Eric Meadows on 5/22/16.
+//  Copyright © 2016 Calmlee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
